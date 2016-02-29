bb9db8838d70945ce52e601c1b52811e0ce46a61
Ivan Samsonov <hronya@gmail.com>
2016-02-29 03:41:45 +0300

    Clean up

--- project.clj
+++ project.clj
@@ -5,10 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure    "1.8.0"]
                  [funcool/catacumba      "0.11.1"]
-                 [cheshire               "5.5.0"]
                  [com.taoensso/timbre    "4.3.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [com.taoensso/carmine   "2.12.2"]
                  [org.immutant/immutant  "2.1.2"]
                  [http-kit               "2.1.19"]]
   :main ^:skip-aot google-cache.core)
